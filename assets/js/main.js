dea972264cc80dd2a9f66dece3278ae9cfa41c12
Akram Mahbub <akramrahman957@gmail.com>
2023-08-19 23:27:57 +0600

    updated with responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 $('.owl-carousel').owlCarousel({
+    items:1,
     loop:true,
     margin:10,
     nav:false,
@@ -7,15 +8,4 @@ $('.owl-carousel').owlCarousel({
     anitmateIn:true,
     anitmateOut:true,
     smartSpeed:500,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
-        }
-    }
 })
